efc0c2507314e985a55688c4899f2d2941c4122d
piotra7r <pseebodop@tutamail.com>
2024-07-07 18:52:05 +0000

    Add files via upload

---
+++ bel_graphemes_and_phonemes.py
@@ -0,0 +1,97 @@
+# Belarusian letters 
+а
+б
+в
+г
+ґ
+д
+е
+ё
+ж
+з
+і
+й
+к
+л
+м
+н
+о
+п
+р
+с
+т
+у
+ў
+ф
+х
+ц
+ч
+ш
+ы
+ь
+э
+ю
+я
+’
+# The letter "ґ" is not used often, and is usually replaced by "г"
+# The apostrophe ("’" or "'") is used to denote a hard consonant before the combination /j/ + vowel; less often, this sign is used to denote abbreviated words, which are most often transmitted without it
+# The letters "яеёю" are used to indicate the combination /j/ + vowel /aɛɔu/ respectively, or the softness of the preceding consonant (except for cases where these letters and the consonant are separated by an apostrophe) before the vowel.
+
+# Belarusian phonemes
+ä
+ɛ
+i
+ɨ
+ɔ
+u
+b
+bʲ
+p
+pʲ
+gʲ
+ɟ
+k
+c
+d
+t
+dz
+dzʲ
+ts
+tsʲ
+ɖʐ
+ʈʂ
+v
+vʲ
+f
+fʲ
+ɣ
+ɣʲ
+x
+xʲ
+z
+zʲ
+s
+sʲ
+ʐ
+ʂ
+m
+mʲ
+n
+nʲ
+r
+l
+lʲ
+w
+j
+# The phoneme /g/ rarely occurs, in some words; and also it replaces /ɣ/ in position after /zsʐʂk/
+# These phonemes do not have a soft pair: /d t ɖʐ ʈʂ ʐ ʂ r w/
+# The /j/ is always soft
+# Labials /pbvfm/ usually are soft only before vowels
+# The phoneme /w/ in Belarusian words usually occurs only in closed syllables after vowels (as /äw ɛw iw ɨw ɔw uw/)
+# Variations of vowels:
+# /ä/: /ä Cʲa/
+# /ɛ/: /ɜ Cʲɛ/
+# /i/: /'i ɪ 'ji- jɪ-/
+# /ɨ/
+# /ɔ/: /ɔ Cʲɔ̈/
+# /u/: /'u ʊ 'Cʲʉ Cʲʊ̈/
